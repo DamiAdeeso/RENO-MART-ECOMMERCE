b422db6fc1b3944fbeab135ba621463b7fb263f7
Oluwadamilola <damilolaadeeso@gmail.com>
2022-06-09 17:50:26 +0100

    payment added

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
-import App from "../frontend/src/App.js";
 
 dotenv.config() 
 
@@ -19,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.length("/api/keys/paypal",(req,res)=>{
+app.get("/api/keys/paypal",(req,res)=>{
   res.send(process.env.PAYPAL_CLIENT_ID || "sb")
 })
 app.use("/api/seed",seedRouter);
